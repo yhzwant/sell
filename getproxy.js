1f202a19af794e8828e424a3c16de01468307d03
yhzwant <116070913+yhzwant@users.noreply.github.com>
2023-10-27 20:49:35 +0800

    Update getproxy.js

--- getproxy.js
+++ getproxy.js
@@ -2,66 +2,38 @@ const axios = require('axios');
 const fs = require('fs');
 
 const proxyURLs = [
-  'https://proxyspace.pro/http.txt',
-  //'http://212.87.213.146:3000/privateproxy2.txt',
-  //'http://212.87.213.146:3000/privateproxy3.txt',
-  //'http://212.87.213.146:3000/privateproxy4.txt'
+  'https://api.proxyscrape.com/?request=displayproxies&proxytype=http',
+  'https://proxyspace.pro/http.txt'
+  // Add more URLs here as needed
 ];
 
-let totalProxiesReceived = 0;
-let totalDuplicates = 0;
-let uniqueProxies = new Set();
-let failedURLs = [];
+const proxyFile = 'proxy.txt';
 
-const proxyfile = process.argv[2];
-
-if (process.argv.length < 3) {
-  console.log('node getproxy.js [Proxyfile]');
-  process.exit();
+async function downloadProxiesFromURL(url) {
+  try {
+    const response = await axios.get(url);
+    const proxies = response.data;
+    return proxies;
+  } catch (error) {
+    console.error(`Error downloading proxies from ${url}:`, error);
+    return '';
+  }
 }
 
 async function downloadProxies() {
-    totalDuplicates = 0;
-    uniqueProxies = new Set();
-    failedURLs = [];
-    totalProxiesReceived = 0;
+  let allProxies = '';
 
   for (const url of proxyURLs) {
-    try {
-      const response = await axios.get(url, { timeout: 8000 });
-
-      const proxies = response.data.split('\n');
-      totalProxiesReceived += proxies.length;
-
-      proxies.forEach(proxy => {
-        if (uniqueProxies.has(proxy)) {
-          totalDuplicates++;
-        } else {
-          uniqueProxies.add(proxy);
-        }
-      });
-    } catch (error) {
-      failedURLs.push(url);
-    }
+    const proxies = await downloadProxiesFromURL(url);
+    allProxies += proxies + '\n';
   }
 
-  const uniqueProxiesArray = Array.from(uniqueProxies);
-  const mergedUniqueProxies = uniqueProxiesArray.join('\n');
-  fs.writeFileSync(proxyfile, mergedUniqueProxies, { flag: 'w' });
-  console.log('');
-  console.log(`Proxies overwritten in file ${proxyfile}`);
-  console.log('');
-  console.log(`Total proxy: ${totalProxiesReceived}`);
-  console.log(`Duplicated proxy: ${totalDuplicates}`);
-  console.log(`Unique proxies: ${uniqueProxies.size}`);
-
-  if (failedURLs.length > 0) {
-    console.log('');
-    console.log('Failed to get proxy:');
-    failedURLs.forEach(url => console.log(url));
+  if (allProxies) {
+    fs.writeFileSync(proxyFile, allProxies, { flag: 'w' });
+    console.log(`Proxies written to ${proxyFile}`);
+  } else {
+    console.error('No proxies were downloaded.');
   }
 }
 
 downloadProxies();
-
-setInterval(downloadProxies, 60 * 2 * 1000);
